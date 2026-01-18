6ed4807dd99c3b5fc19631a656ccd7f41bb5e5cd
sfxcode <tom@sfxcode.com>
2026-01-18 14:36:03 +0100

    chore: update version to 7.18.0 and restructure exports

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -2,15 +2,18 @@ import {defineNuxtConfig} from 'nuxt/config'
 
 export default defineNuxtConfig({
   ssr: true,
+
   modules: [
     '../src/module',
     'nuxt-primevue',
     '@unocss/nuxt',
   ],
+
   typesense: {
     url: process.env.TYPESENSE_URL,
     apiKey: process.env.TYPESENSE_API_KEY,
   },
+
   unocss: {
     uno: true,
     attributify: true,
@@ -22,16 +25,19 @@ export default defineNuxtConfig({
       ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ],
   },
+
   primevue: {
     config: {
       ripple: true,
 
     },
   },
+
   css: [
     'primevue/resources/primevue.css',
     'primeicons/primeicons.css',
     'primevue/resources/themes/tailwind-light/theme.css',
   ],
 
+  compatibilityDate: '2026-01-18',
 })
